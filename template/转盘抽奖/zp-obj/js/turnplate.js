783b9f13caf6c5241069fa3065e7acfa903697a4
mapingdjh <646245532@qq.com>
2016-12-11 22:05:08 +0800

    调整转盘奖品基数时指针不正确

--- template/转盘抽奖/zp-obj/js/turnplate.js
+++ template/转盘抽奖/zp-obj/js/turnplate.js
@@ -37,9 +37,28 @@ var turnplate = {
 				var arc = 2*Math.PI / len;
 				//在给定矩形内清空一个矩形
 			    ctx.clearRect(0,0,422,422);
+				// 控制奖品奇偶数情况下从何处绘制转盘
+				  var point;
+				  if(turnplate.restaraunts.length %2 === 0){
+					  point = Math.PI/2; // 从90度处开始会
+				  }else{
+					  // 奖品奇数时从新处理奖品背景色
+					  var colorLen = turnplate.restaraunts.length;
+					  if(colorLen === 3){
+						 var colorGroupLen = Math.ceil(colorLen / 3);
+						  turnplate.colors = [];
+						  for(var i=0; i<colorGroupLen; i++){
+							turnplate.colors.push("#fff4d6");
+							turnplate.colors.push("#fff");	
+							turnplate.colors.push("#ffebb2");						
+						  }  
+					  }
+					 
+					  point = Math.PI/2 + arc/2; // 从90度+一个转盘一半的度数
+				  }
 				// 开始画圆
 				for(var i = 0; i< len; i++){
-					var angle = this.startAngle + i*arc;
+					var angle = this.startAngle + i*arc + point;
 					ctx.beginPath();
 					ctx.fillStyle = this.colors[i];
 					/** 1、扇形的2中方法*/
@@ -134,21 +153,40 @@ var turnplate = {
 		*/
 		rotateWheel: function(prizeName){
 			var self = this;
-            var prizeIndex = this.getIndexByPName(prizeName);
-			// 每个奖品占有角度
-			var perAngle = 360 / this.restaraunts.length;
-			// 根据奖品计算奖品所在角度(注意rotate插件参数是角度),这里角度是正中间位置，
-			var angle = prizeIndex*perAngle - perAngle/2;
-			// 这里需要区分 奖品在是否在1点钟和2点钟方向(右上角位置)
-			// 因为转盘需要在270正中间停止(指针指向上方位置停止)
-			// 270-angle,不是右上角，转盘选择1800度后(5圈)回到原点，在旋转270-angle度即可到指针正上方停止
-			// 360-angle+270,一点钟或2点钟方向，由于canvas三点中方向是0度，所以需要先转到到0度处(360-angle)
-			// 再转270度即可到达指针正上方
-			angle = angle < 270 ? (270-angle) : (360-angle+270);
+			// 未中奖
+			if(prizeName === -1){
+				if(this.restaraunts.length %2 !== 0){
+					// 奖品配置奇数个
+					var angle = 90;
+				}else{
+					//var angles = 360 / (this.restaraunts.length)+ 360 / (this.restaraunts.length*2);
+					var angle = 360 / (this.restaraunts.length);
+				}
+				
+				prizeName = "未中奖";
+				console.log("未中奖");
+			}else{
+				var prizeIndex = this.getIndexByPName(prizeName);
+				// 每个奖品占有角度
+				var perAngle = 360 / this.restaraunts.length;
+				// 根据奖品计算奖品所在角度(注意rotate插件参数是角度),这里角度是正中间位置，
+				var angle = prizeIndex*perAngle - perAngle/2;
+				if(this.restaraunts.length %2 !== 0){
+					// 绘制转盘是少绘制了转盘一半的度数，所以需要加上一半圆弧度数
+					angle += (360 / (this.restaraunts.length*2));
+				}
+				// 这里需要区分 奖品在是否在1点钟和2点钟方向(右上角位置)
+				// 因为转盘需要在270正中间停止(指针指向上方位置停止)
+				// 270-angle,不是右上角，转盘选择1800度后(5圈)回到原点，在旋转270-angle度即可到指针正上方停止
+				// 360-angle+270,一点钟或2点钟方向，由于canvas三点中方向是0度，所以需要先转到到0度处(360-angle)
+				// 再转270度即可到达指针正上方
+				angle = angle < 270 ? (270-angle) : (360-angle+270);
+			}
+            
 			$("#wheelcanvas").stopRotate();
 			$("#wheelcanvas").rotate({
-				angle: 0,                  // 旋转起始角度
-				animateTo: angle + 1800,   // 旋转结束角度
+				angle: angle,                  // 旋转起始角度
+				animateTo: angle + 1800-90,   // 旋转结束角度,减90修正转盘从90度位置开始绘制
 				duration: 8000,            // 持续时间 
 				callback: function(){      // 旋转结束后回调函数
 					// 将转盘标记为停止状态
